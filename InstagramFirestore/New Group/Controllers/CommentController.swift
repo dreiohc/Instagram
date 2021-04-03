5988e471e4f1441e3e88c671bc78fbfd6d9559ff
Myron Dulay <myrondulay@tuta.io>
2021-04-03 21:22:37 +0800

    add comment input text view for comment vc

--- InstagramFirestore/New Group/Controllers/CommentController.swift
+++ InstagramFirestore/New Group/Controllers/CommentController.swift
@@ -13,12 +13,35 @@ class CommentController: UICollectionViewController {
 
 	// MARK: - Properties
 
+	private lazy var commentInputView: CommentInputAccessoryView = {
+		let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+		let cv = CommentInputAccessoryView(frame: frame)
+		return cv
+	}()
+
 	// MARK: - Lifecycle
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		configureCollectionView()
+	}
 
+	override var inputAccessoryView: UIView? {
+		return commentInputView
+	}
+
+	override var canBecomeFirstResponder: Bool {
+		return true
+	}
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		self.tabBarController?.tabBar.isHidden = true
+	}
+
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		self.tabBarController?.tabBar.isHidden = false
 	}
 
 	// MARK: - Actions
